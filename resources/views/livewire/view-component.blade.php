a48d23fd7faf53a92764f614b9f38466a1a48a3c
Trubaiev Oleksandr <xcrits31@gmail.com>
2024-06-10 17:56:09 +0200

    discount view

--- resources/views/livewire/view-component.blade.php
+++ resources/views/livewire/view-component.blade.php
@@ -44,6 +44,7 @@
                 <td class="border px-4 py-2">{{ $product->name }}</td>
                 <td class="border px-4 py-2">{{ $product->price }}</td>
                 <td class="border px-4 py-2">{{ $product->discount }}</td>
+                <td class="border px-4 py-2">{{ $product->price - ($product->price * $product->discount) }}</td>
                 <td class="border px-2 py-2 text-center">
                     <a href="#" class="no-underline" wire:click.prevent="store({{ $product->id }}, '{{ $product->name}}', {{ $product->price }}, {{$product->discount}} {{Auth::user()->id }})"> <i class="bi bi-cart" style="font-size: 0.75rem;"></i></a>
                 </td>
