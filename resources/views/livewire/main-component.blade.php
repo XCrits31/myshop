25499f46edbef7c53558178d62092a8070cef072
Trubaiev Oleksandr <xcrits31@gmail.com>
2024-06-22 15:33:54 +0200

    end fix

--- resources/views/livewire/main-component.blade.php
+++ resources/views/livewire/main-component.blade.php
@@ -1,4 +1,5 @@
 <div>
+    <h2 class="text-2xl font-bold mb-4"> payments list by periods</h2>
     <form wire:submit.prevent="submit">
         <select wire:model="selectedItem" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
             <option value="">Choose </option>
